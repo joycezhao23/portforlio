44d5cfc0b34db8bdd920cbd3c2ddf366af7d7be0
Xufei Zhao <xzhao@Xufeis-MacBook-Pro.local>
2018-04-30 23:53:37 -0700

    Blog Added

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        pos = self.body.find(".", 100)
+        if pos == -1:
+            pos = len(self.body)
+        return self.body[:(pos+1)]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
